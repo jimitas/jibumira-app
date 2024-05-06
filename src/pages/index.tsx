af8710814701216cad26d4c06df3403c2b602cea
jimitas <nonajun805@gmail.com>
2024-05-06 17:55:23 +0900

    tableを作成した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div>
       <header>
-        <h1>Syllabus Viewer</h1>
+        <h1 className="mt-3 mx-auto text-center">Syllabus Viewer</h1>
         <div className={`${styles.cp_ipselect} ${styles.cp_sl03} overflow-hidden w-11/12 mx-auto text-center`}>
           <select
             className="w-full cursor-pointer text-transparent appearance-none bg-transparent"
@@ -36,12 +36,24 @@ export default function Home() {
         </div>
       </header>
       <main>
-        <h2>{selectedSubject.id}</h2>
-        <ul>
-          <li>{selectedSubject.param_1}</li>
-          <li>{selectedSubject.param_2}</li>
-          <li>{selectedSubject.param_3}</li>
-        </ul>
+        <table className="w-11/12 mx-auto text-center">
+          <tr>
+            <td>教科名</td>
+            <td>{selectedSubject.id}</td>
+          </tr>
+          <tr>
+            <td>単位数</td>
+            <td>{selectedSubject.param_2}</td>
+          </tr>
+          <tr>
+            <td>あああ</td>
+            <td>{selectedSubject.param_3}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
       </main>
     </div>
   );
